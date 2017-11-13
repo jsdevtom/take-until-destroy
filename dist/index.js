d4bab2defaa2c3372918c14d01fdb6efe4ef0528
Tom <contact@jsdevtom.com>
2017-11-13 05:38:57 +0100

    compile 'move index.ts'

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const take_until_destroy_1 = require("./src/take-until-destroy");
+exports.takeUntilDestroy = take_until_destroy_1.takeUntilDestroy;
+const destroyable_decorator_1 = require("./src/destroyable.decorator");
+exports.Destroyable = destroyable_decorator_1.Destroyable;
+//# sourceMappingURL=index.js.map
