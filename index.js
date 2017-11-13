ecb70c7a2ebc0d9bc4f2cc83594eaa656dda5e9f
Tom <contact@jsdevtom.com>
2017-11-13 16:48:07 +0100

    compile index.ts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var take_until_destroy_1 = require("./src/take-until-destroy");
+var take_until_destroy_1 = require("./dist/take-until-destroy");
 exports.takeUntilDestroy = take_until_destroy_1.takeUntilDestroy;
-var destroyable_decorator_1 = require("./src/destroyable.decorator");
+var destroyable_decorator_1 = require("./dist/destroyable.decorator");
 exports.Destroyable = destroyable_decorator_1.Destroyable;
