cb7d31da5b2389cf99191453167b43d54bc5cd61
Tom <contact@jsdevtom.com>
2018-01-13 21:08:02 +0100

    ignore dist folder

--- dist/error-messages.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const ErrorMessages: {
-    NO_NGONDESTROY: string;
-};
--- dist/error-messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorMessages = {
-    NO_NGONDESTROY: 'The angular component inside of which you are trying to unsubscribe from must have the method' +
-        '`ngOnDestroy`. Please see the documentation for details.'
-};
-//# sourceMappingURL=error-messages.js.map
